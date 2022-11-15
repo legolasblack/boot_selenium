faff08667b6013f8ea39317eff32a4e7faead44d
legolasblack <legolas.black.ds@gmail.com>
2022-11-15 17:04:07 -0600

    se implementaron las clases de boot al momento tanto el de palace como el de nuovo llegan solo a querer escribir las fechas y terminan

--- src/main/java/demo/App.java
+++ src/main/java/demo/App.java
@@ -1,22 +1,28 @@
 package demo;
 
-
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        Boot bootCredi7=new Boot(
-            "DIGUTIERREZ",
-            "DGC66818mgr",
-            "https://credi7.ddns.net/",
-            "noviembre"
-            );
-        bootCredi7.setUp();
-        bootCredi7.login();
-        
+public class App {
+    public static void main(String[] args) {
+        /*
+         * Boot bootCredi7=new Boot(
+         * "DIGUTIERREZ",
+         * "DGC66818mgr",
+         * "https://credi7.ddns.net/",
+         * "noviembre"
+         * );
+         * bootCredi7.setUp();
+         * bootCredi7.login_palace();
+         */
+
+        Boot boot_Nuovo = new Boot(
+                "https://app.nuovopay.com/",
+                "dgc@ivfinance.mx",
+                "182hemw3q");
+
+        boot_Nuovo.setUp();
+        boot_Nuovo.login_nuovo();
     }
 }
--- src/main/java/demo/Boot.java
+++ src/main/java/demo/Boot.java
@@ -8,18 +8,23 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Boot {
 
     private WebDriver driver;
-    private String user_palace;
-    private String pass_palace;
-    private String url_palace;
-    private String frasedeautorizacion="¡ Acceso autorizado !";
+    private String user;
+    private String pass;
+    private String url;
     private String mes_inicio;
     private String mes_final;
 
-    public Boot(String user, String pass, String url, String mes){
-        this.user_palace=user;
-        this.pass_palace=pass;
-        this.url_palace=url;
-        this.mes_inicio=mes;
+    public Boot(String url, String user, String pass) {
+        this.user = user;
+        this.pass = pass;
+        this.url = url;
+    }
+
+    public Boot(String url, String user, String pass, String mes) {
+        this.user = user;
+        this.pass = pass;
+        this.url = url;
+        this.mes_inicio = mes;
     }
 
     public void setUp() {
@@ -30,13 +35,13 @@ public class Boot {
             System.setProperty("webdriver.chrome.driver", "./src/test/sources/drivers/chromedriver.exe");
             driver = new ChromeDriver();
             driver.manage().window().maximize();
-            driver.get(this.url_palace);
+            driver.get(this.url);
         } catch (Exception e) {
             System.out.println("existe el siguiente error: " + e);
         }
     }
 
-    public void login() {
+    public void login_palace() {
         // con el tipo de dato web element podemos instaciar elementos del la pagina:
         WebElement userbox = driver.findElement(By.name("ingUsuario"));
         WebElement passbox = driver.findElement(By.name("ingPassword"));
@@ -45,14 +50,14 @@ public class Boot {
         WebElement registro_ventas;
         WebElement financiamientos;
         WebElement first_input_date;
-
+        String frasedeautorizacion = "¡ Acceso autorizado !";
 
         // utilizamos clear para limpiar lo que pueda ver en los inputs
         // utilizamos sendkeys para escribir un string dentro de los inputs
         userbox.clear();
-        userbox.sendKeys(this.user_palace);
+        userbox.sendKeys(this.user);
         passbox.clear();
-        passbox.sendKeys(this.pass_palace);
+        passbox.sendKeys(this.pass);
         try {
             Thread.sleep(2000);
         } catch (Exception e) {
@@ -63,22 +68,73 @@ public class Boot {
         boton_autorizacion = driver.findElement(By.xpath("/html/body/div[1]/div/div[10]/button[1]"));
         System.out.println(label_autorizacion.getText());
         boton_autorizacion.click();
-        registro_ventas =driver.findElement(By.xpath("/html/body/div/div[15]/div[4]/div[1]/a"));
+        registro_ventas = driver.findElement(By.xpath("/html/body/div/div[15]/div[4]/div[1]/a"));
         registro_ventas.click();
-        financiamientos= driver.findElement(By.xpath("/html/body/div/aside[1]/div/nav/ul/li[2]/ul[3]/li/a"));
+        financiamientos = driver.findElement(By.xpath("/html/body/div/aside[1]/div/nav/ul/li[2]/ul[3]/li/a"));
         financiamientos.click();
         System.out.println("hasta aqui llegamos");
     }
-    
 
-    public void ataqueFuerzaBruta(){
+    public void ataqueFuerzaBruta() {
     }
 
+    public void login_nuovo() {
+        WebElement userbox = driver.findElement(By.id("outlined-email-input"));
+        WebElement passbox = driver.findElement(By.id("outlined-adornment-password"));
+        WebElement button_sigin = driver
+                .findElement(By.xpath("/html/body/div[1]/div/main/div/div[1]/div/div/div[2]/div/div/button"));
+        WebElement label_devices;
+        WebElement button_csv;
+        WebElement label_AllDivices;
+        WebElement first_input_date;
+        WebElement second_input_date;
+
+        userbox.clear();
+        userbox.sendKeys(this.user);
+        passbox.clear();
+        passbox.sendKeys(this.pass);
+        button_sigin.click();
+
+        // apartir de aqui hace la autentificacion
+        try {
+            Thread.sleep(5000);
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+        try {
+            System.out.println("Entramos arriba del divace");
+            label_devices = driver
+                    .findElement(By.xpath("//*[@id=" + "'root'" + "]/div/div[1]/div/ul/div[2]/div[2]/div/div/ul/a[2]"));
+            label_devices.click();
+            Thread.sleep(5000);
+            System.out.println("aqui terminamos el click de device");
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+        button_csv = driver
+                .findElement(By.xpath("//*[@id=" + "'root'" + "]/div/main/div[1]/div[2]/div[1]/div[2]/div/div/button"));
+        button_csv.click();
+        label_AllDivices = driver.findElement(By.xpath("//*[@id=" + "'csv-download-menu'" + "]/div[3]/ul/li[1]"));
+        label_AllDivices.click();
+
+        try {
+            Thread.sleep(5000);
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+        first_input_date = driver.findElement(By.id("date-picker-inline-from-date"));
+        first_input_date.clear();
+        first_input_date.sendKeys("01112021");
+        second_input_date = driver.findElement(By.id("date-picker-inline-to-date"));
+        second_input_date.clear();
+        second_input_date.sendKeys("30112021");
+
+        System.out.println("Fin del juego");
+    }
 
 }
 
-
-//tipos de localizadores para elemntos del dom 
+// tipos de localizadores para elemntos del dom
 /*
  * By.id();
  * By.name();
@@ -89,4 +145,7 @@ public class Boot {
  * By.cssSelector();
  * By.xpath();
  * javascript
-*/
+ */
+
+// date-picker-inline-from-date
+// date-picker-inline-to-date
